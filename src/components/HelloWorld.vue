433f40aa6f24c7f7fe76bb974527c70adcc51898
Muhammad Alif Putra Dafi <muhammadalif.pd@gmail.com>
2024-06-04 13:30:43 +0700

    test 2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,17 +5,20 @@ defineProps({
   msg: String,
 })
 
+console.log(import.meta.env.VUE_APP_TESTSTRING);
+console.log(import.meta.env.VITE_TESTSTRING);
 const count = ref(0)
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
+  <!-- <h1>{{import.meta.env.VUE_TESTSTRING}}</h1> -->
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
       Edit
-      <code>components/HelloWorld.vue</code> to test HMR {{import.meta.env.VUE_TESTSTRING}}
+      <code>components/HelloWorld.vue</code> to test HMR 
     </p>
   </div>
 
